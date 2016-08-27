27ed82d98ca55f2e3df101673b8e25fa6a947c8e
Chitra Rajan <thayumaanavan.chitra-rajan@hpe.com>
2016-08-27 10:25:56 +0530

    Method overriding sample

--- inheritance.py
+++ inheritance.py
@@ -4,16 +4,23 @@ class Parent():
         self.last_name=last_name
         self.eye_color=eye_color
         print("Parent Contructor")
+    
+    def show_info(self):
+        print("Last Name: "+self.last_name+";Eye Color: "+self.eye_color)
 
 class Child(Parent):
     def __init__(self,last_name,eye_color,no_of_toys):
         print("Child constructor")
         Parent.__init__(self,last_name,eye_color)
         self.no_of_toys=no_of_toys
+    
+    def show_info(self):
+        print("No. of Toys: "+str(self.no_of_toys))
         
 
 
 
 #parent=Parent("Rajan","black")
-
+#parent.show_info()
 child=Child("Raj","blue",4)
+child.show_info()
